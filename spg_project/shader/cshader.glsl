33c7c46870286cae1e855e887b20d8d2779fe23d
TheodorKnab <32791723+TheodorKnab@users.noreply.github.com>
2020-03-15 18:39:41 +0100

    triLookupTexture working

--- spg_project/shader/cshader.glsl
+++ spg_project/shader/cshader.glsl
@@ -8,32 +8,35 @@ void main()
     ivec3 pixel_coords = ivec3(gl_GlobalInvocationID.xyz);
 
     //vec4 pixel = vec4(float(pixel_coords.x)/dims.x, pixel_coords.y/dims.y, pixel_coords.y/dims.y, 1.0);
-	vec3 pos = vec3(float(pixel_coords.x)/dims.x,float(pixel_coords.y)/dims.y, float(pixel_coords.z)/dims.z);
-	
-	vec2[] pillars = vec2[](vec2(0.333,0.33), vec2(0.66, 0.33), vec2(0.5,0.66));
-	
-	float density = 0;
-	
-	//Pillars
-	for(int i = 0;i < pillars.length(); ++i)
-	{
-		density += clamp(((0.15/ length(pos.xy - vec2(pillars[i].x + 0.1 * sin(10 * pos.z), pillars[i].y + 0.1 * cos(10 * pos.z)))) - 1) * 499999.0,0.0,1.0);		
-	}
-	
-	//Negative Pillar
-	
-	//Outside
-	
-	//Helix
-	
-	//Shelfs
-	
-	//Noise
-
-
-	//vec4 pixel = vec4((pos.x + pos.y + pos.z) /3.0,0,0,1);
-	
-	vec4 pixel = vec4(density,0,0,1);
+    vec3 pos = vec3(float(pixel_coords.x)/dims.x,float(pixel_coords.y)/dims.y, float(pixel_coords.z)/dims.z);
+    
+    vec2[] pillars = vec2[](vec2(0.333,0.33), vec2(0.66, 0.33), vec2(0.5,0.66));
+    
+    float density = 0;
+    
+    //Pillars
+    for(int i = 0;i < pillars.length(); ++i)
+    {
+        density += 0.15 / length(pos.xy - pillars[i].xy) - 1.0;
+        //density += clamp(((0.15 / length(pos.xy - vec2(pillars[i].x + 0.1 * sin(10 * pos.z), pillars[i].y + 0.1 * cos(10 * pos.z)))) - 1) * 499999.0, 0.0, 1.0);        
+    }
+    
+    //Negative Pillar
+    density -= 0.7 / length(pos.xy) - 1.0;
+    
+    //Outside
+    density -= pow(length(pos.xy), 3);
+    
+    //Helix
+    vec2 vec = vec2(cos(pos.z * 3.0), sin(pos.z * 3.0));
+    density += dot(vec, pos.xy);
+    
+    //Shelfs
+    density += cos(pos.z * 20.0);
+    
+    //Noise
+    
+    vec4 pixel = vec4(clamp(density * 499999.0, 0.0, 1.0), 0, 0, 1);
 
     imageStore (tex_output, pixel_coords, pixel);
 }
--- spg_project/shader/gshader.glsl
+++ spg_project/shader/gshader.glsl
@@ -1,4 +1,4 @@
-#version 410 core
+#version 440 core
 layout (points) in;
 layout (triangle_strip, max_vertices = 15) out;
 
@@ -17,8 +17,11 @@ uniform mat4 view;
 uniform mat4 model;
 
 uniform sampler3D densityTexture;
+uniform isamplerBuffer mcTableTexture;
 
-void build_house(vec4 position)
+
+
+void buildHouse(vec4 position)
 {    
     float scale = 0.5;
     vec4 worldPos = inverse(model) * inverse(view) * inverse(projection) * position;
@@ -35,11 +38,13 @@ void build_house(vec4 position)
 
 }
 
-void build_house_s(vec4 position)
+void buildHouseS(vec4 position)
 {    
     float scale = 0.5;
     vec4 worldPos = inverse(model) * inverse(view) * inverse(projection) * position;
-    fColor = vec3(texture(densityTexture, vec3(worldPos.x / 96, worldPos.y / 96, worldPos.z / 256)).rrr); 
+    //fColor = (texelFetch(mcTableTexture, 34)).xxx; 
+    //fColor = vec3(1,1,1);
+    //fColor = vec3(texture(densityTexture, vec3(worldPos.x / 96, worldPos.y / 96, worldPos.z / 256)).rrr); 
     gl_Position = (position * scale + projection * view * model * vec4(-0.1 * scale, -0.1 * scale, 0.0, 0.0)); // 1:bottom-left   
     EmitVertex();   
     gl_Position = (position * scale + projection * view * model * vec4( 0.1 * scale, -0.1 * scale, 0.0, 0.0)); // 2:bottom-right
@@ -52,9 +57,49 @@ void build_house_s(vec4 position)
 
 }
 
-void main() {       
+void placeVertOnEdge(uint edgeNum)
+{
+    // Along this cell edge, where does the density value hit zero?
+    // float str0= dot(cornerAmask0123[edgeNum], input.field0123) + dot(cornerAmask4567[edgeNum], input.field4567);
+    // float str1= dot(cornerBmask0123[edgeNum], input.field0123) + dot(cornerBmask4567[edgeNum], input.field4567);
+    // float t= saturate( str0/(str0 -str1) ); //0..1
+    // use that to get wsCoordand uvwcoords
+
+    vec3 pos_within_cell = vec3(0,0,0); //vec_start[edgeNum] + t * vec_dir[edgeNum]; //[0..1]
+    vec3 vecWsCoord= gs_in[0].wsCoord.xyz + pos_within_cell.xyz;// * wsVoxelSize;
+    gl_Position = vec4(vecWsCoord, 1);
+    EmitVertex();
+    //float3 uvw= input.uvw + ( pos_within_cell*inv_voxelDimMinusOne).xzy;
+
+    //GSOutputoutput;
+    //output.wsCoord_Ambo.xyz= wsCoord;
+    //output.wsCoord_Ambo.w= grad_ambo_tex.SampleLevel(s, uvw, 0).w;
+    //output.wsNormal= ComputeNormal(tex, s, uvw);
+    //return output;
+}
+
+void buildMarchingCube()
+{
+    
+    uint tablePos = gs_in[0].mc_case * 16;
+    
+    if(texelFetch(mcTableTexture, int(tablePos)).x != -1){
+    
+        placeVertOnEdge(uint(texelFetch(mcTableTexture, int(tablePos)).x));
+        tablePos++;
+        placeVertOnEdge(uint(texelFetch(mcTableTexture, int(tablePos)).x));
+        tablePos++;
+        placeVertOnEdge(uint(texelFetch(mcTableTexture, int(tablePos)).x));
+        tablePos++;    
+        EndPrimitive();        
+    }
+}
+
+
+void main() {     
+    buildMarchingCube();   
     if(gs_in[0].mc_case != 0){        
-        build_house_s(gl_in[0].gl_Position);
+       buildHouseS(gl_in[0].gl_Position);
     }
 }
 
